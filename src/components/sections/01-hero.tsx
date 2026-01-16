765ac3361da9293ab40541ed51876f23f4ba4ead
Ethan Lee <ethantlee21@gmail.com>
2026-01-16 21:54:26 +0900

    feat(hero): add video background to hero section

--- src/components/sections/01-hero.tsx
+++ src/components/sections/01-hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { animate, cubicBezier, motion, stagger } from "motion/react";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
 
@@ -15,8 +16,22 @@ const stats = [
 
 export const Hero = () => {
   return (
-    <section className="min-h-svh px-responsive pt-20 pb-8 flex flex-col gap-8">
-      <div className="flex-1 flex flex-col lg:flex-row lg:items-end justify-end lg:justify-between gap-8 md:gap-12">
+    <section className="h-svh px-responsive pt-20 pb-8 flex flex-col gap-8">
+      <div className="flex-1 min-h-0 relative">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, ease: "circOut" }}
+          className="absolute max-md:top-0 md:bottom-0 right-0 w-full lg:w-2/3 border aspect-video bg-muted shadow-lg overflow-hidden mask-b-from-80% mask-b-to-100% mask-r-from-80% mask-r-to-100% -z-10"
+        >
+          <video loop muted autoPlay className="size-full">
+            <source src="/video/dashboard-field-gen.mp4" type="video/mp4" />
+            Browser does not support
+          </video>
+        </motion.div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row lg:items-end justify-end lg:justify-between gap-8 md:gap-12">
         <h1 className="font-serif text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-light tracking-tighter leading-none">
           Bond requests,
           <br />
@@ -64,13 +79,15 @@ type StaggerCharacterProps = {
 };
 
 const StaggerCharacter = ({ children }: StaggerCharacterProps) => {
+  const ease = cubicBezier(0.33, 1.75, 0.68, 1);
+
   useEffect(() => {
     animate(
       `.stagger-character`,
-      { skewX: [-20, 0], fontWeight: [500, 400], opacity: [0.5, 1] },
+      { skewX: [-20, 0], fontWeight: [500, 400], opacity: [0, 1] },
       {
         duration: 0.6,
-        ease: cubicBezier(0.33, 1.75, 0.68, 1),
+        ease,
         delay: stagger(0.03, { startDelay: 0.3, ease: "circIn" }),
       }
     );
@@ -85,7 +102,10 @@ const StaggerCharacter = ({ children }: StaggerCharacterProps) => {
           whileHover={{
             skewX: -20,
           }}
-          className="stagger-character inline-block cursor-grabbing"
+          transition={{
+            ease,
+          }}
+          className="stagger-character inline-block cursor-grabbing opacity-0"
         >
           {char}
         </motion.span>
